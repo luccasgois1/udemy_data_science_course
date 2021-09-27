91be52fa3bac4326bc2bc6f62736ad8ec524dd9c
luccasgois1 <luccasgois1@gmail.com>
2021-09-27 11:27:01 -0300

    Update main.py

--- categorical_variables_exercise/main.py
+++ categorical_variables_exercise/main.py
@@ -12,17 +12,14 @@ df.rename(columns={'n_sell_icecream':'Frequency'},inplace=True)
 
 # Create Bar Char
 df.plot.bar(rot=0, title='Sales')
-# plt.show()
 
 # Create Pie Char
 df['Relative frequency (%)'] = (df/df.sum())*100
 df.plot.pie(y='Relative frequency (%)',autopct='%1.1f%%', title='Sales')
-# plt.show()
 
 # Create Pareto diagram
 df.sort_values(by='Frequency',inplace=True)
 df['Cumulative Frequency %'] = (df.iloc[:,0].cumsum()/df.iloc[:,0].sum())*100
 ax = df.plot.bar(y='Frequency',rot=0, title='Sales')
-# ax1 = ax.twinx()
 df.plot(y='Cumulative Frequency %',color='r', style='.-',secondary_y=True,ax=ax)
 plt.show()
