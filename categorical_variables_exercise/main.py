7b56af4a2b6f588c711b396f552bdb7c7fb08d80
luccasgois1 <luccasgois1@gmail.com>
2021-09-27 11:26:26 -0300

    Update main.py

--- categorical_variables_exercise/main.py
+++ categorical_variables_exercise/main.py
@@ -8,21 +8,21 @@ path_file = 'selling_data.csv'
 df = pd.read_csv(path_file,index_col=0)
 
 # Create Frequency Distribution Table
-df.rename(columns={'n_sell_icecream':'Number of Ice Cream Sold'},inplace=True)
+df.rename(columns={'n_sell_icecream':'Frequency'},inplace=True)
 
 # Create Bar Char
-df.plot.bar(rot=0)
+df.plot.bar(rot=0, title='Sales')
 # plt.show()
 
 # Create Pie Char
 df['Relative frequency (%)'] = (df/df.sum())*100
-df.plot.pie(y='Relative frequency (%)',autopct='%1.1f%%')
+df.plot.pie(y='Relative frequency (%)',autopct='%1.1f%%', title='Sales')
 # plt.show()
 
 # Create Pareto diagram
-df.sort_values(by='Number of Ice Cream Sold',inplace=True)
-df['Cumulative Number of Ice Cream Sold'] = df.iloc[:,0].cumsum()
-ax = df.plot.bar(y='Number of Ice Cream Sold',rot=0)
-df.plot.line(y='Cumulative Number of Ice Cream Sold',color='r',ax=ax)
-ax.legend(['Cumulative Number of Ice Cream Sold','Number of Ice Cream Sold'])
+df.sort_values(by='Frequency',inplace=True)
+df['Cumulative Frequency %'] = (df.iloc[:,0].cumsum()/df.iloc[:,0].sum())*100
+ax = df.plot.bar(y='Frequency',rot=0, title='Sales')
+# ax1 = ax.twinx()
+df.plot(y='Cumulative Frequency %',color='r', style='.-',secondary_y=True,ax=ax)
 plt.show()
